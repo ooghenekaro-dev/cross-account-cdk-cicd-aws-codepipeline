643c792a8b4754873020b4aee4adf054f6dd0b67
ooghenekaro <oghenekaro.oboido@unicard-uk.com>
2025-06-14 15:24:20 +0100

    Initial commit

---
+++ test/cdk-cross-account-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCrossAccountPipeline from '../lib/cdk-cross-account-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cross-account-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCrossAccountPipeline.CdkCrossAccountPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
